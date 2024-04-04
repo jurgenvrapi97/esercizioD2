9f2d5ed9ec149174aa74f3e55972a00f6b8f63f2
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-04-04 16:28:46 +0200

    test

--- src/test/java/com/epicode/U5D1/Esercizio30102023ApplicationTests.java
+++ src/test/java/com/epicode/U5D1/Esercizio30102023ApplicationTests.java
@@ -1,13 +1,67 @@
 package com.epicode.U5D1;
 
+import com.epicode.U5D1.entities.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@SpringBootTest
-class Esercizio30102023ApplicationTests {
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PizzaTest {
+
 
 	@Test
-	void contextLoads() {
+	void createTopping() {
+		Topping topping = new Topping("Cheese", 100, 0.5);
+		assertEquals("Cheese", topping.getName());
+		assertEquals(100, topping.getCalories());
+		assertEquals(0.5, topping.getPrice(), 0.001);
 	}
 
+
+	@Test
+	void createPizza() {
+		Topping topping = new Topping("Cheese", 100, 0.5);
+		List<Topping> toppings = new ArrayList<>();
+		toppings.add(topping);
+		Pizza pizza = new Pizza("Cheese Pizza", toppings, false);
+		assertEquals("Cheese Pizza", pizza.getName());
+		assertFalse(pizza.isXl());
+		assertEquals(1, pizza.getToppingList().size());
+	}
+
+
+	@Test
+	void checkPizzaCalories() {
+		Topping topping1 = new Topping("Cheese", 100, 0.5);
+		Topping topping2 = new Topping("Tomato", 50, 0.3);
+		List<Topping> toppings = new ArrayList<>();
+		toppings.add(topping1);
+		toppings.add(topping2);
+		Pizza pizza = new Pizza("Cheese Tomato Pizza", toppings, false);
+		assertEquals(1162, pizza.getCalories());
+	}
+
+
+	@Test
+	void checkPizzaPrice() {
+		Topping topping1 = new Topping("Cheese", 100, 0.5);
+		Topping topping2 = new Topping("Tomato", 50, 0.3);
+		List<Topping> toppings = new ArrayList<>();
+		toppings.add(topping1);
+		toppings.add(topping2);
+		Pizza pizza = new Pizza("Cheese Tomato Pizza", toppings, false);
+		assertEquals(5.1, pizza.getPrice(), 0.001);
+	}
+
+
+	@ParameterizedTest
+	@ValueSource(strings = {"Cheese", "Tomato", "Mushroom", "Pepperoni", "Olives"})
+	void createMultipleToppings(String toppingName) {
+		Topping topping = new Topping(toppingName, 100, 0.5);
+		assertEquals(toppingName, topping.getName());
+	}
 }
